510897ec7e5488424615d977eed7dad8e0b39110
Gladis <gladyskankwala03@gmail.com>
2026-06-13 09:46:47 +0200

    calcultor handling

--- src/components/Calculater.jsx
+++ src/components/Calculater.jsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
 
+const nums = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+]
 
 function Calculater() {
+
+    const [ input, setInput] = useState('')
+
+    const [click, setClick] = useState(false)
+
+    const handleNumberClick = (num) => {
+        setInput((prev) => prev + num)
+    }
+
     return(
-        <section>
-            <h1>Hello</h1>
+        <section className="flex justify-center p-4 min-h-full bg-gray-100">
+            <div className="bg-black flex flex-col p-6 rounded-2xl shadow-xl w-80">
+                <div className="mb-4">
+                    <input 
+                    type="text" 
+                    placeholder="0"
+                    value={input}
+                    onChange={ (e) => handleNumberClick(e.target.value)}
+                    className="w-full p-3 text-right text-2xl font-mono bg-zinc-800 text-white rounded-lg focus:outline-none"
+                    />
+                </div>
+                <div className="grid grid-colls gap-3">
+                    {nums.map( num => (
+                       <button
+                       key={num}
+                       onClick={() => handleNumberClick(num)}
+                       >
+                       </button>
+                    ))}
+                </div>
+            </div>
         </section>
     )
 
